1225e69237b299d0e824e5ff0b8b746c28f78b71
Attben <mrtobone@gmail.com>
2020-11-12 12:28:57 +0100

    Added API comments.

--- Program.cs
+++ Program.cs
@@ -5,10 +5,18 @@ using System.Linq;
 
 namespace DLTP_assignment_2
 {
+    /* CLASS: Person
+     * PURPOSE: Handle the name, address, phone and email of an entry in an address list
+     */
     class Person
     {
         public string name, address, phone, email;
 
+        /* METHOD: Person (default constructor)
+         * PURPOSE: Construct a Person object by asking the user to input values for each field.
+         * PARAMETERS: none
+         * RETURN VALUE: The new Person that was constructed.
+         */
         public Person()
         {
             Console.Write("  1. ange namn:    ");
@@ -20,11 +28,25 @@ namespace DLTP_assignment_2
             Console.Write("  4. ange email:   ");
             email = Console.ReadLine();
         }
-        public Person(string N, string A, string T, string E)
+        /* METHOD: Person (constructor)
+         * PURPOSE: Construct a Person object.
+         * PARAMETERS: name, address, phone, email - strings to initialize the Person's fields
+         * RETURN VALUE: The new Person that was constructed.
+         */
+        public Person(string name, string address, string phone, string email)
         {
-            name = N; address = A; phone = T; email = E;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+            this.email = email;
         }
 
+        /* METHOD: EditField
+         * PURPOSE: Change the name, address, phone or email to a new value.
+         * PARAMETERS: fieldToChange - The name of the field the caller wants to change.
+         *             newValue - The value the caller wants to change the field to.
+         * RETURN VALUE: 
+         */
         public void EditField(string fieldToChange, string newValue)
         {
             switch (fieldToChange)
@@ -43,9 +65,14 @@ namespace DLTP_assignment_2
     }
     class Program
     {
+        /* METHOD: Main (static)
+         * PURPOSE: Command input loop to let a user interact with an address list.
+         * PARAMETERS: none
+         * RETURN VALUE: none
+         */
         static void Main()
         {
-            List<Person> Dict = LoadAddressList();
+            List<Person> addressList = LoadAddressList();
 
             Console.WriteLine("Hej och välkommen till adresslistan");
             Console.WriteLine("Skriv 'sluta' för att sluta!");
@@ -61,20 +88,20 @@ namespace DLTP_assignment_2
                 else if (command == "ny")
                 {
                     Console.WriteLine("Lägger till ny person...");
-                    Dict.Add(new Person()); //Note: The default constructor will ask for user input.
+                    addressList.Add(new Person()); //Note: The default constructor will ask for user input.
                     Console.WriteLine("Klart.");
                 }
                 else if (command == "ta bort")
                 {
-                    RemovePersonFromList(Dict);
+                    RemovePersonFromList(addressList);
                 }
                 else if (command == "visa")
                 {
-                    PrintAddressList(Dict);
+                    PrintAddressList(addressList);
                 }
                 else if (command == "ändra")
                 {
-                    ChangePersonInList(Dict);
+                    EditPersonInList(addressList);
                 }
                 else
                 {
@@ -83,14 +110,19 @@ namespace DLTP_assignment_2
             } while (command != "sluta");
         }
 
-        private static void ChangePersonInList(List<Person> Dict)
+        /* METHOD: EditPersonInList (static)
+         * PURPOSE: Edit the data of a Person in am address list.
+         * PARAMETERS: addressList - A list of Person objects
+         * RETURN VALUE: none
+         */
+        private static void EditPersonInList(List<Person> addressList)
         {
             Console.Write("Vem vill du ändra (ange namn): ");
             string personToChange = Console.ReadLine();
             int found = -1;
-            for (int i = 0; i < Dict.Count(); i++)
+            for (int i = 0; i < addressList.Count(); i++)
             {
-                if (Dict[i].name == personToChange) found = i;
+                if (addressList[i].name == personToChange) found = i;
             }
             if (found == -1)
             {
@@ -102,26 +134,34 @@ namespace DLTP_assignment_2
                 string fieldToChange = Console.ReadLine();
                 Console.Write("Vad vill du ändra {0} på {1} till: ", fieldToChange, personToChange);
                 string newValue = Console.ReadLine();
-                Dict[found].EditField(fieldToChange, newValue);
+                addressList[found].EditField(fieldToChange, newValue);
             }
         }
-
-        private static void PrintAddressList(List<Person> Dict)
+        /* METHOD: PrintAddressList (static)
+         * PURPOSE: Print the name, address, phone number and email of every Person in an address list
+         * PARAMETERS: addressList - A List of Person objects
+         * RETURN VALUE: none
+         */
+        private static void PrintAddressList(List<Person> addressList)
         {
-            for (int i = 0; i < Dict.Count(); i++)
+            for (int i = 0; i < addressList.Count(); i++)
             {
-                Dict[i].Print();
+                addressList[i].Print();
             }
         }
-
-        private static void RemovePersonFromList(List<Person> Dict)
+        /* METHOD: RemovePersonFromList (static)
+         * PURPOSE: Remove a Person from an address list
+         * PARAMETERS: addressList - A List of Person objects
+         * RETURN VALUE: none
+         */
+        private static void RemovePersonFromList(List<Person> addressList)
         {
             Console.Write("Vem vill du ta bort (ange namn): ");
             string personToRemove = Console.ReadLine();
             int found = -1;
-            for (int i = 0; i < Dict.Count(); i++)
+            for (int i = 0; i < addressList.Count(); i++)
             {
-                if (Dict[i].name == personToRemove) found = i;
+                if (addressList[i].name == personToRemove) found = i;
             }
             if (found == -1)
             {
@@ -129,14 +169,18 @@ namespace DLTP_assignment_2
             }
             else
             {
-                Dict.RemoveAt(found);
+                addressList.RemoveAt(found);
             }
         }
-
+        /* METHOD: LoadAddressList (static)
+         * PURPOSE: Load a List of Person objects from a file.
+         * PARAMETERS: none
+         * RETURN VALUE: The List<Person> that was loaded.
+         */
         private static List<Person> LoadAddressList()
         {
             Console.Write("Laddar adresslistan ... ");
-            List<Person> Dict = new List<Person>();
+            List<Person> addressList = new List<Person>();
             using (StreamReader fileStream = new StreamReader(@"..\..\address.lis"))
             {
                 while (fileStream.Peek() >= 0)
@@ -146,11 +190,11 @@ namespace DLTP_assignment_2
                     string[] word = line.Split('#');
                     // Console.WriteLine("{0}, {1}, {2}, {3}", word[0], word[1], word[2], word[3]);
                     Person P = new Person(word[0], word[1], word[2], word[3]);
-                    Dict.Add(P);
+                    addressList.Add(P);
                 }
             }
             Console.WriteLine("klart!");
-            return Dict;
+            return addressList;
         }
     }
 }
